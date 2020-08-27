05f3ffa9c129f9391307214ba531c2135fa78045
David Molina <davidmolina2810@gmail.com>
2020-08-27 23:33:30 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,5 @@
 # Code your solution here!
-require 'pry'
+
 def random_num
   rand(1..6)
 end
@@ -7,18 +7,11 @@ end
 def get_user_guess
   puts "Enter a number between 1 and 6"
   num = gets.chomp
-  #if num.length == 1
-  #  num = num.to_i
-  #else
-  #  num
-  #end
 end
 
 def run_guessing_game
   num = random_num
-  binding.pry
   user_num = get_user_guess
-  #binding.pry
   if user_num.length == 1
     user_num = user_num.to_i
     if user_num == num
